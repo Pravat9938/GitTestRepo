e009341ff1554d8af18bc471c9e05ba93a0c8a83
Pravat9938 <pravat.pks99383028@gmail.com>
2023-08-11 10:00:38 +0530

    fails service

---
+++ src/main/java/com/nit/SpringBootProj01DevelopApp125Application.java
@@ -0,0 +1,13 @@
+package com.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProj01DevelopApp125Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProj01DevelopApp125Application.class, args);
+	}
+
+}
